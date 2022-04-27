1316b61bddca6c4cadf936e56bb75c798536ed37
jgrk28 <jacobkaplan28@gmail.com>
2022-04-27 22:39:26 +0800

    Connect to postgres database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const { Client } = require('pg')
+const client = new Client()
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'UltiResults' });
+router.get('/', async (req, res, next) => {
+  //Use env variables in launch.json to connect to db
+  await client.connect()
+  const data = await client.query('SELECT * FROM teams')
+  var teams = data.rows
+  res.render('index', { title: 'UltiResults', teams});
+  await client.end()
 });
 
 module.exports = router;
