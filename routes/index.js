65ef52b14395de8ada9fbf10d927d0ff795aa0a1
jgrk28 <jacobkaplan28@gmail.com>
2022-05-01 10:52:35 +0800

    Display tournament data on the frontend

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,23 @@ const client = new Client()
 router.get('/', async (req, res, next) => {
   //Use env variables in launch.json to connect to db
   await client.connect()
-  const data = await client.query('SELECT * FROM teams')
-  var teams = data.rows
-  res.render('index', { title: 'UltiResults', teams});
+
+  //Get current, past, and upcoming tournaments
+  //TODO reformat date from database
+  const currentDate = new Date().toISOString().slice(0, 10);
+  const currentQueryString = `SELECT * FROM tournaments WHERE 
+start_date <= '${currentDate}' AND end_date >= '${currentDate}'`
+  const upcomingQueryString = `SELECT * FROM tournaments WHERE start_date > '${currentDate}'`
+  const pastQueryString = `SELECT * FROM tournaments WHERE end_date < '${currentDate}'`
+  const currentTournamentsQuery = await client.query(currentQueryString);
+  const upcomingTournamentsQuery = await client.query(upcomingQueryString);
+  const pastTournamentsQuery = await client.query(pastQueryString);
+  const currentTournaments = currentTournamentsQuery.rows;
+  const upcomingTournaments = upcomingTournamentsQuery.rows;
+  const pastTournaments = pastTournamentsQuery.rows;
+
+  //Render index page passing in the data from the database
+  res.render('index', { title: 'UltiResults', currentTournaments, upcomingTournaments, pastTournaments});
   await client.end()
 });
 
