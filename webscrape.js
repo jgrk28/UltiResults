eda2e8bae3bdb52fdfc27b1613c582a8a0d1832d
jgrk28 <jacobkaplan28@gmail.com>
2022-05-02 23:15:09 +0800

    Add individual game schedule scraper

--- webscrape.js
+++ webscrape.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 const cheerio = require('cheerio');
+const { Client } = require('pg');
 
 // Division enum maps each division to its integer id
 class Division {
@@ -36,6 +37,40 @@ class Division {
 				throw `${division} is not a division`;
 		}
 	}
+
+	//Return appropriate division object for given ID
+	static createFromId(divId) {
+		switch (divId) {
+			case 1:
+				return this.D1_women;
+			case 2:
+				return this.D3_women;
+			case 3:
+				return this.D1_men;
+			case 4:
+				return this.D3_men;
+			default:
+				throw `${divId} is not a division ID`;
+		}
+	}
+}
+
+// Team class provides functionality related to the teams database table
+class Team {
+	//Gets the ID of given team if not in table adds to table
+	static async getId(teamName, division, database) {
+		const selectQuery = `SELECT id FROM teams WHERE usau_name = 
+'${teamName}' AND division_id = ${division.id}`;
+		var data = await database.query(selectQuery);
+		if (data.rowCount == 0) {
+			//TODO try to fetch twitter handle from twitter API
+			const insertQuery = `INSERT INTO teams(division_id, usau_name) 
+VALUES(${division.id}, '${teamName}') RETURNING id`;
+			//Returns new id as if we selected it
+			data = await database.query(insertQuery);
+		}
+		return data.rows[0].id;
+	}
 }
 
 //Turns dates of the form m(m)/dd into yyyy-mm-dd
@@ -54,10 +89,50 @@ const formatTournamentInsert = (date, division, event_name, location, event_url)
 VALUES (${division_id}, '${start_date}', '${end_date}', '${event_name}', '${location}', '${event_url}')`;
 }
 
+const convertTime12to24 = (time12h) => {
+	const [time, amPm] = time12h.split(' ');
+	let [hours, minutes] = time.split(':');
+	if (hours === '12') {
+	  hours = '00';
+	}
+	if (amPm === 'PM') {
+	  hours = parseInt(hours, 10) + 12;
+	}
+  
+	return `${hours}:${minutes}`;
+}
+
+const formatGameInsert = async (tournamentId, date, time, team1, team2, division, database) => {
+	//Cut off final word which is always the seed to leave just the team name
+	const team1NoSeed = team1.substring(0, team1.lastIndexOf(" "));
+	const team2NoSeed = team2.substring(0, team2.lastIndexOf(" "));
+
+	const team1Id = await Team.getId(team1NoSeed, division, database);
+	const team2Id = await Team.getId(team2NoSeed, division, database);
+	//TODO make sure this doesnt break near new year
+	const currYear = new Date().getFullYear();
+
+	//date is in the form 'DOW M(M)/D(D)' e.g. 'Sat 4/30'
+	const monthDay = date.split(" ")[1];
+	const [month, day] = monthDay.split("/");
+	const zeroIndexedMonth = parseInt(month, 10) - 1;
+
+	//TODO adjust to timezone of tournament
+	//time is in the form 'H(H):MM AM/PM' e.g. '9:00 AM'
+	const time24h = convertTime12to24(time);
+	const [hours, minutes] = time24h.split(":");
+
+	const databaseDate = new Date(currYear, zeroIndexedMonth, day, hours, minutes);
+	const databaseTime = databaseDate.toISOString().slice(0, 19);
+	return `INSERT INTO games (tournament_id, team1_id, team2_id, start_time) 
+	VALUES (${tournamentId}, ${team1Id}, ${team2Id}, '${databaseTime}')`;;
+}
+
 //Scrape tournaments from USA ultimate website and insert into tournament table
 //TODO safer db connect/unconnect and better error handling
 const getTournaments = async () => {
 	try {
+		//TODO move connection outside and pass in when needed
 		//connect to database
 		const { Client } = require('pg');
 		const client = new Client();
@@ -96,4 +171,63 @@ const getTournaments = async () => {
 	}
 };
 
-getTournaments()
+//Scrape tournament schedule from given USA ultimate url
+//url - string representing the base url for the tournament info page
+//division - Division object determining which division to get the schedule for
+const getSchedule = async (tournamentId, database) => {
+	const selectString = `SELECT * FROM tournaments WHERE id = ${tournamentId}`
+	const tournamentQuery = await database.query(selectString);
+	const tournament = tournamentQuery.rows[0];
+
+	const division = Division.createFromId(tournament.division_id);
+	const url = tournament.url;
+
+	try {
+		var fullUrl = url + "/schedule";
+		switch (division) {
+			case Division.D1_women:
+			case Division.D3_women:
+				fullUrl = fullUrl + "/Women/CollegeWomen/";
+				break;
+			case Division.D1_men:
+			case Division.D3_men:
+				fullUrl = fullUrl + "/Men/CollegeMen/";
+				break;
+			default:
+				`division ${division.id} is unknown`
+		}
+
+		//get html for USA Ultimate schedule site
+		const { data } = await axios.get(fullUrl);
+
+		const $ = cheerio.load(data);
+		//each table row represents one tournament (both past and future)
+        const games = $('.scores_table > tbody > tr:not(:first-child)').toArray();
+		for (const game of games) {
+			const day = $(game).find("td:eq(0)").text().trim();
+			const time = $(game).find("td:eq(1)").text().trim();
+			const team1 = $(game).find("td:eq(3)").text().trim();
+			const team2 = $(game).find("td:eq(4)").text().trim();
+			try {
+				const insert_query = 
+					await formatGameInsert(tournamentId, day, time, team1, team2, division, database);
+				await database.query(insert_query);
+			} catch(error) {
+				console.log(error);
+			}
+		}
+	} catch (error) {
+		throw error;
+	}
+};
+
+//For testing only needed to run async top level
+const runTest = async() => {
+	const database = new Client();
+	await database.connect();
+	//getTournaments()
+	await getSchedule(5, database);
+	await database.end();
+}
+
+runTest();
